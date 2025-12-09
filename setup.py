69f13965d108f8410fdf96be745195afa1a0ea5c
amartyachatterjee <amartya.chatterjee@gmail.com>
2025-12-09 17:35:57 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='sentiment_analyzer',
     version='0.1.0',
     description='Performs basic sentiment analysis (Vader, TextBlob, Blobber and an ensemble) of input text',
-    # long_description=open('README.md').read(),
+    long_description=open('README.md').read(),
     author='Amartya Chatterjee',
     author_email='amartya.chatterjee@gmail.com',
     license='MIT',
@@ -20,3 +20,4 @@ setup(
         'textblob'
     ]
 )
+
